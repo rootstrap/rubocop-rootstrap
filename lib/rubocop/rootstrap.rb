d4f22094e532fc320095c43bbe7998a383ee284b
Juan Aparicio <apariciojuan30@gmail.com>
2020-05-27 11:01:37 -0300

    Initial commit

---
+++ lib/rubocop/rootstrap.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require 'rubocop/rootstrap/version'
+
+module Rubocop
+  module Rootstrap
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
