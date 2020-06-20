90e8e90b74d1a82c5f5c6c7257f29b7d159b3687
Juan Manuel Ramallo <juanmanuelramallo@hey.com>
2020-06-20 18:37:26 -0300

    Adding config files (#1)
    * Adding config files
    
    * Updating runtime dependency
    
    * Fix rails config yml

--- spec/rubocop/rootstrap_spec.rb
+++ spec/rubocop/rootstrap_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe Rubocop::Rootstrap do
   it 'has a version number' do
     expect(Rubocop::Rootstrap::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
